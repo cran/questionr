cb30d2bf73478fb3c8240fd1782fe9f522fd1f1a
Julien Barnier <julien.barnier@ens-lyon.fr>
2016-03-15 15:10:41 +0000

    version 0.5

--- R/table.multi.R
+++ R/table.multi.R
@@ -57,7 +57,7 @@ multi.table <- function(df, true.codes=NULL, weights=NULL, digits=1, freq=TRUE) 
     res <- cbind(res, pourc)
     colnames(res) <- c("n","%multi")
   }
-  if(!is.null(digits)) res <- round(res, digits)
+  if (!is.null(digits)) res <- round(res, digits)
   return(res)
 }
 
@@ -73,6 +73,7 @@ multi.table <- function(df, true.codes=NULL, weights=NULL, digits=1, freq=TRUE) 
 ##' @param digits number of digits to keep in the output
 ##' @param freq display percentages 
 ##' @param tfreq type of percentages to compute ("row" or "col")
+##' @param n if \code{TRUE}, and freq is \code{TRUE}, display number of observations per row or column.
 ##' @param ... arguments passed to \code{multi.table}
 ##' @details
 ##' See the \code{multi.table} help page for details on handling of the multiple
@@ -98,35 +99,39 @@ multi.table <- function(df, true.codes=NULL, weights=NULL, digits=1, freq=TRUE) 
 ##' cross.multi.table(df[,c("jazz", "rock","electronic")], df$sex, true.codes=list("Y"), freq=TRUE)
 ##' ## Row percentages based on respondants
 ##' cross.multi.table(df[,c("jazz", "rock","electronic")], 
-##'                   df$sex, true.codes=list("Y"), freq=TRUE, tfreq="row")
+##'                   df$sex, true.codes=list("Y"), freq=TRUE, tfreq="row", n=TRUE)
 ##' @export
  
-cross.multi.table <- function(df, crossvar, weights=NULL, digits=1, freq=FALSE, tfreq="col", ...) {
+cross.multi.table <- function(df, crossvar, weights = NULL, digits = 1, freq = FALSE, tfreq = "col", n = FALSE, ...) {
   tmp <- factor(crossvar)
-  if(is.null(weights))
-      res <- simplify2array(by(df, tmp, multi.table, freq=FALSE, digits=NULL, ...))
+  if (is.null(weights))
+      res <- simplify2array(by(df, tmp, multi.table, freq = FALSE, digits = NULL, ...))
   else {
       ## (Not very elegant) fix when weights is provided
       df <- cbind(weights, df)
       res <- by(df, tmp, function(d) {
           tmpw <- d[,1]
           tmpd <- d[,-1]
-          multi.table(tmpd, weights=tmpw, freq=FALSE, digits=NULL, ...)
+          multi.table(tmpd, weights = tmpw, freq = FALSE, digits = NULL, ...)
       })
       res <- simplify2array(res)
   }
   ## Column percentages
-  if (freq & tfreq!="row") {
+  if (freq & tfreq != "row") {
     if(is.null(weights)) totals <- table(tmp)
     else totals <- wtd.table(tmp, weights=weights)
     totals <- totals[colnames(res)]
+    if (n) eff <- apply(res,2,sum)
     res <- sweep(res, 2, totals, FUN="/") * 100
+    if (n) res <- rbind(res, n=eff)
   }
   ## Row percentages
-  if (freq & tfreq=="row") {
+  if (freq & tfreq == "row") {
     totals <- multi.table(df, weights=weights, freq=FALSE, digits=NULL, ...)
     totals <- totals[rownames(res)]
+    if (n) eff <- apply(res,1,sum)    
     res <- sweep(res, 1, totals, FUN="/") * 100
+    if (n) res <- cbind(res, n=eff)
   }
   return(round(res, digits))
 }
@@ -144,7 +149,7 @@ cross.multi.table <- function(df, crossvar, weights=NULL, digits=1, freq=FALSE, 
 ##' "red/green/yellow", etc. This function splits the variable into as many variables
 ##' as the number of different choices. Each of these variables as a 1 or 0 value
 ##' corresponding to the choice of this answer. They are returned as a data frame.
-##' ##' @return Returns a data frame.
+##' @return Returns a data frame.
 ##' @seealso \code{\link[questionr]{multi.table}}
 ##' @examples
 ##' v <- c("red/blue","green","red/green","blue/red")
@@ -153,19 +158,32 @@ cross.multi.table <- function(df, crossvar, weights=NULL, digits=1, freq=FALSE, 
 ##' multi.table(multi.split(v))
 ##' @export
 
-multi.split <- function (var, split.char="/", mnames = NULL) {
+multi.split <- function(var, split.char="/", mnames = NULL) {
   vname <- deparse(substitute(var))
   lev <- levels(factor(var))
-  lev <- unique(unlist(strsplit(lev, split.char)))
+  lev <- unique(unlist(strsplit(lev, split.char, fixed = TRUE)))
+  split.char <- escape_regex(split.char)
   if (is.null(mnames)) 
     mnames <- gsub(" ", "_", paste(vname, lev, sep = "."))
   else mnames <- paste(vname, mnames, sep = ".")
   result <- matrix(data = 0, nrow = length(var), ncol = length(lev))
   char.var <- as.character(var)
   for (i in 1:length(lev)) {
-    result[grep(lev[i], char.var, fixed = TRUE), i] <- 1
+    pattern <- sprintf("(^|%s)%s(%s|$)", split.char, escape_regex(lev[i]), split.char)
+    result[grep(pattern, char.var), i] <- 1
   }
   result <- data.frame(result)
   colnames(result) <- mnames
   result
 }
+
+
+##' Escape regex special chars
+##' Code directly taken from Hmisc::escapeRegex
+##' @param s string to escape regex special chars from
+
+escape_regex <- function(s) {
+  gsub("([.|()\\^{}+$*?]|\\[|\\])", "\\\\\\1", s)
+}
+
+
